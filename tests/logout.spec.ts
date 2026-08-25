e899123ce15f6bc13ab973658d69761cbad8bda8
Sushma0905 <deepika.sushma1@gmail.com>
2026-08-24 22:55:46 -0500

    Added logout fun

---
+++ tests/logout.spec.ts
@@ -0,0 +1,5 @@
+import { test, expect } from '@playwright/test';
+test('Verify that a user can log out successfully', async ({ page }) => {
+  // Navigate to the login page
+  console.log("Logout successfully");   })
+  
